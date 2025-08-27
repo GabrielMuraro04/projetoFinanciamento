76581d8fa57d062c77f7a62520e35f95625f092c
Gabriel Muraro <gabrielmurarodev@hotmail.com>
2025-08-26 22:27:24 -0300

    Implementa classe Financiamento com atributos e métodos principais

--- src/model/Financiamento.java
+++ src/model/Financiamento.java
@@ -1,4 +1,34 @@
 package model;
 
-public class Financiamento {
+public abstract class Financiamento {
+
+    private double valorImovel;
+    private int prazoFinanciamento;
+    private double taxaJurosAnual;
+
+    public Financiamento(double valorImovel, int prazoFinanciamento, double taxaJurosAnual) {
+        this.valorImovel = valorImovel;
+        this.prazoFinanciamento = prazoFinanciamento;
+        this.taxaJurosAnual = taxaJurosAnual;
+    }
+
+    public double getValorImovel() {
+        return valorImovel;
+    }
+
+    public int getPrazoFinanciamento () {
+        return prazoFinanciamento;
+    }
+
+    public double getTaxaJurosAnual() {
+        return taxaJurosAnual;
+    }
+
+    public abstract double calcularPagamentoMensal();
+
+    public double calcularTotalPagamento() {
+        return calcularPagamentoMensal() * prazoFinanciamento * 12;
+    }
+
+    public abstract void exibirResumo();
 }
